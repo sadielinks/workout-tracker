7dd981d823e6a7078614c78ab727af8ef49a4fe9
Sadie Sial <sadiesial@gmail.com>
2021-12-02 00:04:04 -0600

    entered schema day + starting excercises

--- models/Workout.js
+++ models/Workout.js
@@ -1,9 +1,25 @@
 // built with https://mongoosejs.com/docs/guide.html
 
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+// mongoose virtuals https://mongoosejs.com/docs/tutorials/virtuals.html
+const opts = { toJSON: { virtuals: true } };
 
-})
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now,
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: 'Please enter an exercise.'
+                }
+            }
+        ]
+}
+)
