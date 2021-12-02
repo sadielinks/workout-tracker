b6ac7af275447996108e299703aca6f7960852d1
Sadie Sial <sadiesial@gmail.com>
2021-12-01 23:05:45 -0600

    added add. skeleton server.js middleware

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const path = require('path');
+const app = express();
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 const Workout = require('./models')
 
 
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}!!!!`);
+});
