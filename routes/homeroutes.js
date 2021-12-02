a60ac1528fc4ca9ea97614652d2b2df3fe48ed3d
Sadie Sial <sadiesial@gmail.com>
2021-12-02 00:47:07 -0600

    home route with index.html added

--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
 const path = require('path');
 
+// GET routes to follow:
+
+// home route
+router.get("/", (req, res) => {
+    // route to index.html
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
