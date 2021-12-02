9240754cb5b4dfc9973767939e2ad9ff30018be2
Sadie Sial <sadiesial@gmail.com>
2021-12-02 00:59:26 -0600

    commented the functions from public/api.js to help code routes

---
+++ routes/api.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout.js");
+
+// using public/api.js to build following routes
+
+// getLastWorkout() (public/api.js line 2)
+
+
+// addExercise(data) (public/api.js line 13)
+
+
+// createWorkout(data = {}) (public/api.js line 26)
+
+
+// getWorkoutsInRange() (public/api.js line 38)
+
+
+module.exports = router;
