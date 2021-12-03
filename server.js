434c132549031784237b588ace8bac060c23eedf
Sadie Sial <sadiesial@gmail.com>
2021-12-02 18:18:48 -0600

    mongoose connection to server.js

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const Workout = require('./models/Workout')
+const db = require('./models')
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
+});
 
 
 app.listen(PORT, () => {
