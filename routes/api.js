bcfda0f75fde45b605a35f86acc43cea96f627a7
Sadie Sial <sadiesial@gmail.com>
2021-12-02 18:10:38 -0600

    started workouts by id but trouble with push

--- routes/api.js
+++ routes/api.js
@@ -15,7 +15,16 @@ router.get('/api/workouts', (req, res) => {
 })
 
 // addExercise(data) (public/api.js line 13)
-
+router.put('/api/workouts/:id', (req, res) => {
+    console.log(req.body);
+    // https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
+    Workout.findByIdAndUpdate(req.params.id, {
+        // push to exercise array
+        $push: {
+            exercises: req.body
+          }
+    })
+});
 
 // createWorkout(data = {}) (public/api.js line 26)
 
