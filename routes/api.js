a57a401987102e8d407912f2754aada8e78925ef
Sadie Sial <sadiesial@gmail.com>
2021-12-03 21:17:08 -0600

    sum aggregation added to getLastWorkout router.get

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,27 @@
 const router = require('express').Router();
-const Workout = require('../models/Workout');
+const db = require('../models');
 
 // using public/api.js to build following routes
 
 // getLastWorkout() (public/api.js line 2)
-router.get('/api/workouts', (req, res) => {
- // https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/
-    Workout.aggregate()
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+router.get('/api/workouts/', (req, res) => {
+ // sum aggregation + addFields: https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/
+    db.Workout.aggregate(
+      [
+        {
+          $addFields: { 
+              // based off of seed.js
+              day: "$day",
+              // 
+              totalDuration: { $sum: "$exercises.duration" }
+            }
+        }
+    ])
 });
 
 // addExercise(data) (public/api.js line 13)
-router.put('/api/workout/:id', (req, res) => {
-\    // https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
+router.put('/api/workouts/:id', (req, res) => {
+   // https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
     Workout.findByIdAndUpdate(req.params.id, {
         // push to exercise array
         $push: {
@@ -33,7 +37,7 @@ router.put('/api/workout/:id', (req, res) => {
 });
 
 // createWorkout(data = {}) (public/api.js line 26)
-router.post('/api/workouts', (req, res) => {
+router.post('/api/workouts/', (req, res) => {
     Workout.create({})
       .then(dbWorkout => {
         res.json(dbWorkout);
@@ -45,7 +49,7 @@ router.post('/api/workouts', (req, res) => {
 
 
 // getWorkoutsInRange() (public/api.js line 38)
-router.get('/api/workout/range', (req, res) => {
+router.get('/api/workouts/range/', (req, res) => {
     Workout.find({})
     .sort('-day')
     .limit(7)
