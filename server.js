f2bca1d93c09782d23e2ddc005fa491a8584e5f7
Sadie Sial <sadiesial@gmail.com>
2021-12-03 20:55:56 -0600

    BCS support edit to app.use order

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const logger = require('morgan');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(logger('dev'));
 
 const db = require('./models')
 
