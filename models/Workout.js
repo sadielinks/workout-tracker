54ba1842c86e8761c8980beed14816fa5dd6c268
Sadie Sial <sadiesial@gmail.com>
2021-12-04 09:20:10 -0600

    changed Workout.js to lowercase per BCS rec.

--- models/Workout.js
+++ models/Workout.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema(
+const workoutSchema = new Schema(
     {
         day: {
             type: Date,
@@ -49,6 +49,6 @@ const WorkoutSchema = new Schema(
 },
 );
 
-const Workout = mongoose.model('Workout', WorkoutSchema);
+const Workout = mongoose.model('Workout', workoutSchema);
 
 module.exports = Workout;
