8009d4489cd3d8f8bf11696275bac1c73511f8e8
Sadie Sial <sadiesial@gmail.com>
2021-12-04 09:21:50 -0600

    updated to lowercase mongoose path to models/workout.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 
 const db = require('./models')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Workout', 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', 
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
