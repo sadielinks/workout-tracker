9d84d286da036e2a66a31dfed90f0f9bd7ff5976
Sadie Sial <sadiesial@gmail.com>
2021-12-04 09:33:47 -0600

    seperaterated the PUT methods that BCS had me place as one

--- routes/api.js
+++ routes/api.js
@@ -24,36 +24,56 @@ const db = require('../models');
 // });
 
 router.get("/workouts/", (req, res) => {
-	db.Workout.find({})
-		.then((workout) => {
-			res.json(workout);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
+  db.Workout.find({})
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 // addExercise(data) (public/api.js line 13)
-router.put('/api/workouts/:id', (req, res) => {
-	const id = req.params.id;
-	const body = req.body;
+router.put('workouts/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
 
   // findIdAndUpdate: https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
-	db.Workout.findOneAndUpdate({_id: id}, {
+  db.Workout.findOneAndUpdate({ _id: id }, {
     $push: {
       exercises: body
     }
   })
-		.then((data) => {
-			res.json(data);
-		})
-		.catch((err) => {
-			res.status(400).json(err);
-		});
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
+router.put("/workouts/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
 
-
+  db.Workout.findOneAndUpdate({ _id: id }, {
+    $push: {
+      exercises: body
+    }
+  },
+  // findIdAndUpdate properties:
+    { 
+      new: true, 
+      runValidators: true 
+    }
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 
 
